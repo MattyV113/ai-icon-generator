f75c82ca9ad955d960acbd8665f4a47ab8920d75
MattyV113 <88385205+MattyV113@users.noreply.github.com>
2023-08-11 10:00:37 -0400

    adding credits functionality

--- src/server/api/routers/generate.ts
+++ src/server/api/routers/generate.ts
@@ -1,16 +1,40 @@
+/* eslint-disable @typescript-eslint/no-unsafe-assignment */
+/* eslint-disable @typescript-eslint/no-unsafe-call */
+/* eslint-disable @typescript-eslint/no-unsafe-member-access */
 import { z } from "zod";
 
-import { createTRPCRouter, publicProcedure } from "../trpc";
+import { createTRPCRouter, protectedProcedure } from "../trpc";
+import { TRPCError } from "@trpc/server";
+import { TRPCContextProps } from "@trpc/react-query/shared";
 
 export const generateRouter = createTRPCRouter({
-  generateIcon: publicProcedure
+  generateIcon: protectedProcedure
     .input(
       z.object({
         prompt: z.string(),
       })
     )
-    .mutation(({ ctx, input }) => {
-      console.log(input.prompt);
+    .mutation(async ({ ctx, input }) => {
+      const { count } = await ctx.prisma.user.updateMany({
+        where: {
+          id: ctx.session.user.id,
+          credits: {
+            gte: 1,
+          },
+        },
+        data: {
+          credits: {
+            decrement: 1,
+          },
+        },
+      });
+
+      if (count <= 0) {
+        throw new TRPCError({
+          code: "BAD_REQUEST",
+          message: "You do not have enough credits.",
+        });
+      }
       return {
         message: "Success",
       };
